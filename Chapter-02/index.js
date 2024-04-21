1205cad32e38022950d0a93dde2656569d7a86c1
Zeeshan-Haider-Soomro <159009736+Zeeshan-Haider-Soomro@users.noreply.github.com>
2024-04-22 00:31:36 +0500

    chapter-02-03

---
+++ Chapter-02/index.js
@@ -0,0 +1,34 @@
+
+var userName;
+var myName = "ZEESHAN HAIDER SOOMRO";
+var message = "Assalam o Alaikum";
+alert(message);
+
+// Bio Data
+var userName = "Zeeshan Haider Soomro";
+var userAge = "22 year old";
+var userCourse = "Web and development";
+alert(userName);
+alert(userAge);
+alert(userCourse);
+
+// QUESTION 05
+var pizza = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+alert(pizza);
+
+// QUESTION 06
+var email = "zh7774440@gmail.com";
+alert("my email is " + email);
+
+// QUEASTION 07
+var book = "A smarter\n way to learn JavaScript.";
+alert("I'm trying to learn from the book" + book);    
+
+// QUESTION 08
+var skill = "Yah! I can write HTML content through JavaScript";
+document.write(skill);
+
+// QUESTION 09
+var theEnd = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+alert(theEnd);
+document.write(theEnd);
---
+++ Chapter-03/index.js
@@ -0,0 +1,17 @@
+var userAge = "I'm a " + 22 + " year old.";
+alert(userAge);
+
+var track = "You Have Visited this site " + 234 + " times";
+alert(track);
+
+// QUESTION 03
+var birthYear = "2001";
+document.write("My birth year is " + birthYear + " <br>Data type of my decleared variable is number</br>");
+console.log(birthYear);
+console.log(typeof (birthYear));
+
+// QUESTION 04
+var visiterName = "Zeeshan Haider Soomro";
+var productTitle = "T-Shirt";
+var Quantity = 23;
+document.write(visiterName , " ordered " , Quantity , productTitle + "(s) on xyz clothings store.")
